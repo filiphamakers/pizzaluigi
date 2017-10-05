e67d491fabe42be93971cf43da4b45c5b047f27f
VDABHEVERLEE\filip.hamakers <filiphamakers@hotmail.com>
2017-10-05 13:42:23 +0200

    Aanpassingen conform 17.1 p. 46

--- WebContent/WEB-INF/JSP/pizzas.jsp
+++ WebContent/WEB-INF/JSP/pizzas.jsp
@@ -11,11 +11,11 @@
 	<c:import url="/WEB-INF/JSP/menu.jsp" />
 	<h1>Pizza's</h1>
 	<ul class="zebra">
-		<c:forEach var="entry" items="${pizzas}">
-			<li>${entry.key}: ${entry.value.naam} &euro;${entry.value.prijs}<c:if
-					test="${entry.value.pikant}"> pikant</c:if> <c:url
+		<c:forEach var="pizza" items="${pizzas}">
+			<li>${pizza.id}:<c:out value="${pizza.naam}" />
+				&euro;${pizza.prijs} ${pizza.pikant?"pikant":"niet pikant"} <c:url
 					value="/pizzas/detail.htm" var="detailURL">
-					<c:param name="id" value="${entry.key}" />
+					<c:param name="id" value="${pizza.id}" />
 				</c:url> <a href="${detailURL}">Detail</a>
 			</li>
 		</c:forEach>
--- src/be/vdab/servlets/PizzasServlet.java
+++ src/be/vdab/servlets/PizzasServlet.java
@@ -1,9 +1,6 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.ServletException;
@@ -12,19 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import be.vdab.entities.Pizza;
+import be.vdab.repositories.PizzaRepository;
 
-/**
- * Servlet implementation class PizzasServlet
- */
-@WebServlet("/pizzas.htm")
+@WebServlet(urlPatterns = "/pizzas.htm")
 public class PizzasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final PizzaRepository pizzaRepository = new PizzaRepository();
 	private static final String VIEW = "/WEB-INF/JSP/pizzas.jsp";
 	private static final String PIZZAS_REQUESTS = "pizzasRequests";
-	
-	
-	
+
 	@Override
 	public void init() throws ServletException {
 		this.getServletContext().setAttribute(PIZZAS_REQUESTS, new AtomicInteger());
@@ -34,12 +27,7 @@ public class PizzasServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		((AtomicInteger) this.getServletContext().getAttribute(PIZZAS_REQUESTS)).incrementAndGet();
-		Map<Long, Pizza> pizzas = new LinkedHashMap<>();
-		pizzas.put(12L, new Pizza(12, "Prosciutto", BigDecimal.valueOf(4), true));
-		pizzas.put(14L, new Pizza(14, "Margherita", BigDecimal.valueOf(5), false));
-		pizzas.put(17L, new Pizza(17, "Calzone", BigDecimal.valueOf(4), false));
-		pizzas.put(23L, new Pizza(23, "Fungi & Olive", BigDecimal.valueOf(5), false));
-		request.setAttribute("pizzas", pizzas);
+		request.setAttribute("pizzas", pizzaRepository.findAll());
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
